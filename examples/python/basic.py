deeb6dc3d00a4cd86743ad3f8b374f63b6734192
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2024-09-25 19:47:08 -0500

    Feat: Cambio en configuración.
    Configuración para ejecución de ejemmplo medikit collection.

--- examples/python/basic.py
+++ examples/python/basic.py
@@ -25,13 +25,13 @@ if __name__ == "__main__":
     # Now it's time for configuration!
     # load_config could be used to load configuration instead of doing it here with code.
     # If load_config is used in-code configuration will also work - most recent changes will add to previous ones.
-    game.load_config("../../scenarios/basic.cfg")
-    # game.load_config("../../scenarios/health_gathering.cfg")
+    # game.load_config("../../scenarios/basic.cfg")
+    game.load_config("../../scenarios/health_gathering.cfg")
 
     # Sets path to additional resources wad file which is basically your scenario wad.
     # If not specified default maps will be used and it's pretty much useless... unless you want to play good old Doom.
-    game.set_doom_scenario_path(os.path.join(vzd.scenarios_path, "basic.wad"))
-    # game.set_doom_scenario_path(os.path.join(vzd.scenarios_path, "health_gathering.wad"))
+    # game.set_doom_scenario_path(os.path.join(vzd.scenarios_path, "basic.wad"))
+    game.set_doom_scenario_path(os.path.join(vzd.scenarios_path, "health_gathering.wad"))
 
     # Sets map to start (scenario .wad files can contain many maps).
     game.set_doom_map("map01")
@@ -79,8 +79,8 @@ if __name__ == "__main__":
     # game.add_available_button(vzd.Button.ATTACK)
     # Or by setting them all at once:
     game.set_available_buttons(
-        [vzd.Button.MOVE_LEFT, vzd.Button.MOVE_RIGHT, vzd.Button.ATTACK]
-        # [vzd.Button.TURN_LEFT, vzd.Button.TURN_RIGHT, vzd.Button.MOVE_FORWARD]
+        # [vzd.Button.MOVE_LEFT, vzd.Button.MOVE_RIGHT, vzd.Button.ATTACK]
+        [vzd.Button.TURN_LEFT, vzd.Button.TURN_RIGHT, vzd.Button.MOVE_FORWARD]
     )
     # Buttons that will be used can be also checked by:
     print("Available buttons:", [b.name for b in game.get_available_buttons()])
@@ -136,11 +136,13 @@ if __name__ == "__main__":
     actions = [[True, False, False], [False, True, False], [False, False, True]]
 
     # Run this many episodes
-    episodes = 10
+    episodes = 10000000000000000000000000000000000000
 
     # Sets time that will pause the engine after each action (in seconds)
     # Without this everything would go too fast for you to keep track of what's happening.
-    sleep_time = 1.0 / vzd.DEFAULT_TICRATE  # = 0.028
+    # sleep_time = 1.0 / vzd.DEFAULT_TICRATE  # = 0.028
+
+    sleep_time = 0
 
     for i in range(episodes):
         print(f"Episode #{i + 1}")
